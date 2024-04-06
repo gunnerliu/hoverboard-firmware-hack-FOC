36424dd0f28b7547313dcaa77a8844b0e372cc66
ArchLiu <1038151951@qq.com>
2024-04-06 13:10:36 +0800

    add

--- Inc/config.h
+++ Inc/config.h
@@ -332,7 +332,8 @@
     #define FLASH_WRITE_KEY      0x1002  // Flash memory writing key. Change this key to ignore the input calibrations from the flash memory and use the ones in config.h
   #endif
 
-  // #define TANK_STEERING              // use for tank steering, each input controls each wheel 
+  //  开启坦克转向，两个轮子分别设置速度
+   #define TANK_STEERING              // use for tank steering, each input controls each wheel
   // #define SUPPORT_BUTTONS_LEFT       // use left sensor board cable for button inputs.  Disable DEBUG_SERIAL_USART2!
   // #define SUPPORT_BUTTONS_RIGHT      // use right sensor board cable for button inputs. Disable DEBUG_SERIAL_USART3!
 #endif
